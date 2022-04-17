30edb05af7ae0900f1ddbb8b3fc89a3578ea6458
yosuke ota <otameshiyo23@gmail.com>
2022-04-18 08:06:12 +0900

    Update demo

--- docs/.vuepress/components/playground/state/serialize.js
+++ docs/.vuepress/components/playground/state/serialize.js
@@ -2,17 +2,18 @@ import pako from "pako"
 
 /**
  * Serialize a given state as a base64 string.
- * @param {State} state The state to serialize.
+ * @param {State} saveData The state to serialize.
  * @returns {string} The serialized string.
  */
 export function serializeState(saveData) {
     const jsonString = JSON.stringify(saveData)
-    const compressedString = pako.deflate(jsonString, { to: "string" })
+
+    const uint8Arr = new TextEncoder().encode(jsonString)
+    const compressedString = String.fromCharCode(...pako.deflate(uint8Arr))
     const base64 =
         (typeof window !== "undefined" && window.btoa(compressedString)) ||
         compressedString
 
-    //eslint-disable-next-line no-console
     console.log(
         `The compress rate of serialized string: ${(
             (100 * base64.length) /
